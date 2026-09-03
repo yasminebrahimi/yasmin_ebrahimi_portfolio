d124fe1505330b0d1d14a43a0754e083056b0677
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-09-03 15:49:57 +0300

    Add decorative background blobs to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,21 @@ import Hero from "../sections/Hero";
 
 function Home() {
   return (
-    <>
+    <div className="relative overflow-hidden">
+      {/* Decorative background blobs */}
+      <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+        {/* top right, start of the site */}
+        <div className="absolute -right-24 top-0 h-96 w-96 rounded-full bg-brand/25 blur-3xl" />
+        {/* left, between Hero and About */}
+        <div className="absolute -left-32 top-[14%] h-80 w-80 rounded-full bg-blue-400/25 blur-3xl" />
+        {/* right, next to Experience */}
+        <div className="absolute -right-32 top-[38%] h-96 w-96 rounded-full bg-brand/20 blur-3xl" />
+        {/* left, next to Projects */}
+        <div className="absolute -left-40 top-[58%] h-96 w-96 rounded-full bg-blue-300/20 blur-3xl" />
+        {/* right, next to Contact - mix of purple and blue */}
+        <div className="absolute -right-24 top-[80%] h-96 w-96 rounded-full bg-gradient-to-br from-brand/25 to-blue-400/25 blur-3xl" />
+      </div>
+
       <Navbar />
       <main className="flex-1">
         <Hero />
@@ -20,7 +34,7 @@ function Home() {
         <Contact />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
